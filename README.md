deda8097dd0f47da10a83f9aa351addb3a8f333f
Thomas A Caswell <tcaswell@gmail.com>
2015-12-23 19:56:21 -0500

    BLD: add files for making stand-alone project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Document
+
+`Document` is a light-weight sub-class of the built in `dict` which is
+1. read only
+2. has `.` (`getattr`) access
+3. knows it's `_name`
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='document',
+      version='1.0.0',
+      author='Brookhaven National Laboratory',
+      py_modules=['document'],
+      description='A read-only dottable dictionary',
+      url='http://github.com/NSLS-II/document',
+      requires=['six']
+      )
