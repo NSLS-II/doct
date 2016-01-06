996d38113658d97d4cec04357b8d84766df724c0
Thomas A Caswell <tcaswell@bnl.gov>
2016-01-06 13:32:50 -0500

    API: rename module document -> doct

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='document',
+setup(name='doct',
       version='1.0.0',
       author='Brookhaven National Laboratory',
-      py_modules=['document'],
+      py_modules=['doct'],
       description='A read-only dottable dictionary',
-      url='http://github.com/NSLS-II/document',
+      url='http://github.com/NSLS-II/doct',
       requires=['six']
       )
