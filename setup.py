6c0c6b811d2adcb1b7738ee3fe68445ded6c5f17
Eric Dill <edill@bnl.gov>
2016-06-03 14:30:17 -0400

    requires->install_requires. Add license key

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
 from setuptools import setup
 
-setup(name='doct',
-      version='1.0.0',
-      author='Brookhaven National Laboratory',
-      py_modules=['doct'],
-      description='A read-only dottable dictionary',
-      url='http://github.com/NSLS-II/doct',
-      requires=['six', 'humanize']
-      )
+setup(
+    name='doct',
+    version='1.0.0',
+    author='Brookhaven National Laboratory',
+    license='BSD 3-Clause'
+    py_modules=['doct'],
+    description='A read-only dottable dictionary',
+    url='http://github.com/NSLS-II/doct',
+    install_requires=['six', 'humanize']
+)
