ff3be2845a6510614d79ce9998d25d28eee1a421
Eric Dill <edill@bnl.gov>
2016-06-03 14:47:34 -0400

    syntax error in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='doct',
     version='1.0.0',
     author='Brookhaven National Laboratory',
-    license='BSD 3-Clause'
+    license='BSD 3-Clause',
     py_modules=['doct'],
     description='A read-only dottable dictionary',
     url='http://github.com/NSLS-II/doct',
